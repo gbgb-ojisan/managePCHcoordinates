9dc4d1c7ff03da2ecefef0b767974e22665bbcdf
rija765 <hoge@piyo.com>
2018-10-07 12:23:49 +0900

    added CSV function

--- get_pch_coordinate.py
+++ get_pch_coordinate.py
@@ -2,6 +2,7 @@
 
 import urllib
 from bs4 import BeautifulSoup
+import csv
 
 # URL
 # Season 4, October channel
@@ -13,16 +14,36 @@ html = urllib.request.urlopen(url)
 # HTML Parser
 soup = BeautifulSoup(html, 'lxml')
 
+# Open a csv file
+f = open('pch_season4_OctCh.csv', 'w')
+fieldNames = ['pchId', 'groupId', 'name', 'rarity', 'category', 'like', 'imgPath']
+writer = csv.DictWriter(f, fieldnames=fieldNames)
+writer.writeheader()
 # Get div.coordinate-list
 divs = soup.find_all('div', attrs={"class": "coordinate-list"})
 for div in divs:
     # divはコーデ一式に対応するhtml要素
     print(div.attrs)
+    groupId = div['id']
     lis = div.find('ul').find_all('li')
     for li in lis:
+        # Get the attributes of a item.
         pchid = li.find('div', attrs={"class": "-shoulder"}).get_text()
-        print(pchid)
+        category = li.find('div', attrs={"class": "-category"}).get_text()
+        name = li.find('div', attrs={"class": "-title"}).get_text()
+        rarity = li.find('div', attrs={"class": "-rarity"}).get_text()
+        like = li.find('div', attrs={"class": "-like"}).get_text()
         imgUrl = 'https://prichan.jp' + li.find('img')['data-src']
         saveName = './imgs/' + pchid + '.jpg'
-        print(imgUrl)
-        urllib.request.urlretrieve(imgUrl, saveName)
+        writer.writerow({'pchId': pchid,
+                         'groupId': groupId,
+                         'name': name,
+                         'rarity': rarity,
+                         'like': like,
+                         'category': category,
+                         'imgPath': saveName})
+f.close()
+                         
+
+#        print(imgUrl)
+#        urllib.request.urlretrieve(imgUrl, saveName)
