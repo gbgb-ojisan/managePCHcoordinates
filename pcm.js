85beb42a43cf1e4204e84172666a1021a267cbff
rija765 <hoge@piyo.com>
2018-10-09 00:36:44 +0900

    Created html and js files.

---
+++ pcm.js
@@ -0,0 +1,11 @@
+﻿var csvFile = "pch_season4_OctCh.csv";
+
+function Init(){
+	console.log("Init");
+	Papa.parse(csvFile, {
+		download: true,
+		complete: function(results){
+			console.log(results);
+		}
+	});
+}
