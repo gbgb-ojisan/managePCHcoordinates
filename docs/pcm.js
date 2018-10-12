35165695ba06ed9094ef6db956978dc9dad9999e
Ryoya Watanabe <ryoya@RyoyanoMacBook.local>
2018-10-12 23:07:20 +0900

    First useful html

--- docs/pcm.js
+++ docs/pcm.js
@@ -3,6 +3,13 @@
 function Init(){
 jQuery(function($){
 	console.log("Init");
+	/* Check localstorage already exists */
+	if(!localStorage.getItem('existData')){
+		/* First access*/
+		localStorage.setItem('existData', 1);
+	}else{
+		var isExistData = 1;
+	}
 	/* Table */
 	var $table = $('<table>');
 	var $thead = $('<thead>');
@@ -14,7 +21,6 @@ jQuery(function($){
 	$tr.append('<td>Image</td>');
 	$thead.append($tr);
 	$table.append($thead);
-	var $checkbox = $('<button type="checkbox"></button>');
 	var $tbody = $('<tbody>');
 
 	Papa.parse(csvFile, {
@@ -22,12 +28,34 @@ jQuery(function($){
 		download: true,
 		header: true,
 		complete: function(results){
-			$.each(results, function(idx,val){
+			console.log(results);
+			var data = results['data'];
+			var nogroupIdx = 0;
+			$.each(data, function(idx,val){
 				console.log(idx);
-				console.log(val);
-				var $trBody = $('<tr>');
-				$trBody.append($checkbox);
-				$trBody.append($('<td>').html(val['pchId']));
+				/* Set pchId */
+				var pchId = val['pchId'];
+				/* Read local data */
+				var cbAttr = ' type="checkbox" onChange="setLsData(this)"';
+				if(isExistData === 1){
+					var isChecked = localStorage.getItem(pchId);
+					if(isChecked){
+						/* Checked coordinate*/
+						cbAttr = ' type="checkbox" checked onChange="setLsData(this)"';
+					}
+				}
+				/* Set groupID */
+				var groupId = val['groupId'];
+				if(!groupId){
+					groupId = ""+nogroupIdx;
+					nogroupIdx += 1;
+				}
+				var $trBody = $('<tr>',
+					{id: pchId,
+					 "class": groupId
+					});
+				$trBody.append($('<td>').append('<input'+cbAttr+'></input>'));
+				$trBody.append($('<td>').html(pchId));
 				$trBody.append($('<td>').html(val['groupId']));
 				$trBody.append($('<td>').html(val['name']));
 				$tbody.append($trBody);
@@ -36,5 +64,13 @@ jQuery(function($){
 			$('#main').html($table);
 		}
 	});
+});
+}
+
+function setLsData(obj){
+jQuery(function($){
+	var pchId = $(obj).closest('tr').attr('id');
+	localStorage.setItem(pchId, 1);
+	console.log(pchId);
 });
 }
