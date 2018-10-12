f0592ac54fc1aa18911f62678834ab5fd0632390
Ryoya Watanabe <ryoya@RyoyanoMacBook.local>
2018-10-12 11:14:44 +0900

    test

--- docs/pcm.js
+++ docs/pcm.js
@@ -1,13 +1,17 @@
 ﻿var csvFile = "pch_season4_OctCh.csv";
 
 function Init(){
+jQuery(function($){
 	console.log("Init");
 	Papa.parse(csvFile, {
 		encoding: 'UTF-8',
 		download: true,
 		header: true,
 		complete: function(results){
-
+			$.each(results, function(idx,val){
+				console.log(idx+val);
+			});
 		}
 	});
+});
 }
