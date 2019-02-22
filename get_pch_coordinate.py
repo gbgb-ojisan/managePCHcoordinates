c93df0f2b362ae0a5fefbdecacb045143f923060
gbgb-ojisan <gbgb-ojisan@twitter>
2019-02-22 14:36:18 +0900

    Update script and airanju csv.

--- get_pch_coordinate.py
+++ get_pch_coordinate.py
@@ -3,12 +3,19 @@
 import urllib
 from bs4 import BeautifulSoup
 import csv
+import re
 
 # -----------------------------------------------------
 # URL
-url = "https://prichan.jp/items/6th_2.html"
+url = "https://prichan.jp/items/selection.html#6-P1"
 # CSV filename
-filename = 'pch_season6_FebCh.csv'
+filename = 'pch_selection_airanju.csv'
+# セレクションの場合，過去のコーデと同一のページに追記されるため，
+# divのidで絞り込む必要がある
+isSelection = 1
+if isSelection:
+    interestId = '6-P'
+
 # -----------------------------------------------------
 
 # Access to the URL
@@ -19,12 +26,16 @@ soup = BeautifulSoup(html, 'lxml')
 
 fieldNames = ['pchId', 'groupId', 'name', 'rarity', 'category', 'like', 'imgPath', 'imgUrl']
 # Open a csv file
-# ATTN: Wondowsで実行するとSJISのCSVとして書き込まれるので注意（encoding指定が効かない)
+# ATTN: Windowsで実行するとSJISのCSVとして書き込まれるので注意（encoding指定が効かない)
 with open(filename, 'w', encoding='utf-8') as f:
     writer = csv.DictWriter(f, fieldnames=fieldNames)
     writer.writeheader()
     # Get div.coordinate-list
-    divs = soup.find_all('div', attrs={"class": "coordinate-list"})
+    if isSelection:
+        divs = soup.find_all('div', attrs={"id": re.compile(interestId), "class": "coordinate-list"})
+    else:
+        divs = soup.find_all('div', attrs={"class": "coordinate-list"})
+    
     for div in divs:
         # divはコーデ一式に対応するhtml要素
         print(div.attrs)
